2f92cb9a250d44e38951e00739280e26914b1be2
Floryan Simar <floryansimar@gmail.com>
2025-02-25 10:23:42 +0100

    feat: added autocmd to close vim if neotree is only open

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -69,6 +69,37 @@ return {
           end
         end,
       })
+
+      vim.api.nvim_create_autocmd("BufEnter", {
+        desc = "Quit vim if more than one window is open and only sidebar windows are list",
+        callback = function()
+          local wins = vim.api.nvim_tabpage_list_wins(0)
+          -- Both neo-tree and aerial will auto-quit if there is only a single window left
+          if #wins <= 1 then
+            return
+          end
+          local sidebar_fts = { aerial = true, ["neo-tree"] = true }
+          for _, winid in ipairs(wins) do
+            if vim.api.nvim_win_is_valid(winid) then
+              local bufnr = vim.api.nvim_win_get_buf(winid)
+              local filetype = vim.bo[bufnr].filetype
+              -- If any visible windows are not sidebars, early return
+              if not sidebar_fts[filetype] then
+                return
+                -- If the visible window is a sidebar
+              else
+                -- only count filetypes once, so remove a found sidebar from the detection
+                sidebar_fts[filetype] = nil
+              end
+            end
+          end
+          if #vim.api.nvim_list_tabpages() > 1 then
+            vim.cmd.tabclose()
+          else
+            vim.cmd.qall()
+          end
+        end,
+      })
     end,
     opts = {
       sources = { "filesystem", "buffers", "git_status" },
