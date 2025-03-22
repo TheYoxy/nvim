b02cedba112fc2904fed2a36c25e0b11e8ac3e3b
Floryan Simar <floryansimar@gmail.com>
2025-03-22 12:29:30 +0100

    feat(material): enabled all material

--- lua/plugins/material.lua
+++ lua/plugins/material.lua
@@ -1,31 +1,33 @@
+---@module "lazy"
 ---@type LazySpec
 return {
   "marko-cerovac/material.nvim",
+  --- @module "material"
   opts = {
     plugins = { -- Uncomment the plugins that you use to highlight them
       -- Available plugins:
-      -- "coc",
-      -- "colorful-winsep",
+      "coc",
+      "colorful-winsep",
       "dap",
-      -- "dashboard",
-      -- "eyeliner",
-      -- "fidget",
-      -- "flash",
+      "dashboard",
+      "eyeliner",
+      "fidget",
+      "flash",
       "gitsigns",
-      -- "harpoon",
-      -- "hop",
+      "harpoon",
+      "hop",
       "illuminate",
-      -- "indent-blankline",
-      -- "lspsaga",
-      -- "mini",
-      -- "neogit",
+      "indent-blankline",
+      "lspsaga",
+      "mini",
+      "neogit",
       "neotest",
       "neo-tree",
-      -- "neorg",
+      "neorg",
       "noice",
       "nvim-cmp",
-      -- "nvim-navic",
-      -- "nvim-tree",
+      "nvim-navic",
+      "nvim-tree",
       "nvim-web-devicons",
       "rainbow-delimiters",
       "sneak",
@@ -35,5 +37,15 @@ return {
       "nvim-notify",
     },
     theme = "material-stealth",
+    styles = {
+      comments = {
+        italic = true,
+      },
+    },
+    disable = {
+      background = true,
+      term_colors = true,
+    },
+    lualine_style = "stealth",
   },
 }
