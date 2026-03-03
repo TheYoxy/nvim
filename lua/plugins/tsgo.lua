b9d6429136953658cc98167352b34fd6558885ce
Floryan Simar <floryansimar@gmail.com>
2026-03-03 16:26:45 +0100

    feat: added tsgo support

---
+++ lua/plugins/tsgo.lua
@@ -0,0 +1,91 @@
+return {
+  -- correctly setup lspconfig
+  {
+    "mason-org/mason.nvim",
+    opts = { ensure_installed = { "tsgo" } },
+  },
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      -- make sure mason installs the server
+      servers = {
+        vtsls = {
+          enabled = false,
+        },
+        tsgo = {
+          enabled = true,
+          keys = {
+            {
+              "gD",
+              function()
+                local win = vim.api.nvim_get_current_win()
+                local params = vim.lsp.util.make_position_params(win, "utf-16")
+                LazyVim.lsp.execute({
+                  command = "typescript.goToSourceDefinition",
+                  arguments = { params.textDocument.uri, params.position },
+                  open = true,
+                })
+              end,
+              desc = "Goto Source Definition",
+            },
+            {
+              "gR",
+              function()
+                LazyVim.lsp.execute({
+                  command = "typescript.findAllFileReferences",
+                  arguments = { vim.uri_from_bufnr(0) },
+                  open = true,
+                })
+              end,
+              desc = "File References",
+            },
+            {
+              "<leader>ri",
+              LazyVim.lsp.action["source.removeUnused.ts"],
+              desc = "Remove unused imports",
+            },
+            {
+              "<leader>rI",
+              LazyVim.lsp.action["source.organizeImports"],
+              desc = "Organize Imports",
+            },
+            {
+              "<leader>lo",
+              LazyVim.lsp.action["source.addMissingImports.ts"],
+              desc = "Add missing imports",
+            },
+            {
+              "<leader>lD",
+              LazyVim.lsp.action["source.fixAll.ts"],
+              desc = "Fix all diagnostics",
+            },
+            {
+              "<leader>rN",
+              LazyVim.lsp.action["refactor.move.newFile"],
+              desc = "Move to new file",
+            },
+            {
+              "<leader>rV",
+              function()
+                LazyVim.lsp.execute({ command = "typescript.selectTypeScriptVersion" })
+              end,
+              desc = "Select TS workspace version",
+            },
+          },
+        },
+      },
+      setup = {
+        --- @deprecated -- tsserver renamed to ts_ls but not yet released, so keep this for now
+        --- the proper approach is to check the nvim-lspconfig release version when it's released to determine the server name dynamically
+        tsserver = function()
+          -- disable tsserver
+          return true
+        end,
+        ts_ls = function()
+          -- disable tsserver
+          return true
+        end,
+      },
+    },
+  },
+}
--- queries/typescript/injections.scm
+++ queries/typescript/injections.scm
@@ -8,3 +8,13 @@
   function: (identifier) @injection.language (#eq? @injection.language "sql")
   arguments: (arguments (template_string (string_fragment (#set! injection.language "sql"))))
   )
+
+((call_expression
+  function: (identifier) @tag
+  arguments: (template_string (#set! injection.language "xml")) @template)
+ (#match? @tag "^xml$")
+)
+(call_expression
+  function: (identifier) @injection.language (#eq? @injection.language "xml")
+  arguments: (arguments (template_string (string_fragment (#set! injection.language "xml"))))
+  )
